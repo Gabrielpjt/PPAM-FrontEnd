e13800b77ce212045f60a35379e685f0316eefec
AufarR <aufaralazhar@gmail.com>
2024-05-31 02:26:08 +0700

    Fix profile

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,31 +1,43 @@
 import React, { useContext } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
 import Navbar from '../components/ui/Navbar';
 import { UsersContext } from '../contexts/UserContext';
+import Header from '../components/ui/Header';
 
 const ProfileScreen = ({ navigation }) => {
   const { users } = useContext(UsersContext);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.profileContainer}>
-        <Image 
-          source={{ uri: `https://simanis.stei.itb.ac.id/fodex/download/${users.foto}` }} 
-          style={styles.profileImage} 
-        />
-        <View style={styles.detailsContainer}>
-          <Text style={styles.detailText}>Nama Pengguna: {users.nama}</Text>
-          <Text style={styles.detailText}>Nama Toko: {users.nama_toko}</Text>
-          <Text style={styles.detailText}>Alamat Toko: {users.alamat}</Text>
-          <Text style={styles.detailText}>No. Telepon: {users.no_telp}</Text>
+    <SafeAreaView style={styles.safeArea}>
+      <Header title="Profile" />
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.profileContainer}>
+          <Image 
+            source={{ uri: `https://simanis.stei.itb.ac.id/fodex/download/${users.foto}` }} 
+            style={styles.profileImage} 
+          />
+          <View style={styles.detailsContainer}>
+            <Text style={styles.detailText}>Nama Pengguna: {users.nama}</Text>
+            <Text style={styles.detailText}>Nama Toko: {users.nama_toko}</Text>
+            <Text style={styles.detailText}>Alamat Toko: {users.alamat}</Text>
+            <Text style={styles.detailText}>No. Telepon: {users.no_telp}</Text>
+          </View>
         </View>
-      </View>
+      </ScrollView>
       <Navbar navigation={navigation} />
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+  },
+  scrollView: {
+    flex: 1,
+    marginBottom: 50,
+  },
   container: {
     flex: 1,
     justifyContent: 'space-between',
